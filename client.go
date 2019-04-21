1589faba60cbe45ee9cbe64c7386117bb108e0ee
SPie <d.spie@pm.me>
2019-04-22 01:51:08 +0200

    started with client request method

--- client.go
+++ client.go
@@ -1,1 +1,57 @@
 package wlgo
+
+import (
+    "errors"
+    "io/ioutil"
+    "net/http"
+)
+
+type Client interface {
+    GetApiEndpoint() (string)
+    GetSenderId() (string)
+    Request(action string, parameters map[string]string) ([]byte, error)
+}
+
+type WLClient struct {
+    apiEndpoint string
+    senderId string
+}
+
+func NewClient(apiEndpoint string, senderId string) (WLClient, error) {
+    if len(apiEndpoint) < 1 {
+        return WLClient{}, errors.New("Empty API endpoint")
+    }
+    if len(senderId) < 1 {
+        return WLClient{}, errors.New("Empty sender id")
+    }
+
+    return WLClient{apiEndpoint: apiEndpoint, senderId: senderId}, nil
+}
+
+func (wlClient WLClient) GetApiEndpoint() (string) {
+    return wlClient.apiEndpoint
+}
+
+func (wlClient WLClient) GetSenderId() (string) {
+    return wlClient.senderId
+}
+
+func (wlClient WLClient) Request(action string, parameters map[string]string) ([]byte, error) {
+    if len(action) < 1 {
+        return nil, errors.New("Empty action")
+    }
+
+    response, _ := http.Get(wlClient.buildURL(action, parameters))
+
+    defer response.Body.Close()
+    return ioutil.ReadAll(response.Body)
+}
+
+func (wlClient WLClient) buildURL(action string, parameters map[string]string) (string) {
+    url := wlClient.GetApiEndpoint() + "/" + action + "?sender=" + wlClient.GetSenderId()
+    for key, value := range parameters {
+        url += "&" + key + "=" + value
+    }
+
+    return url
+}
--- client_test.go
+++ client_test.go
@@ -1,1 +1,73 @@
 package wlgo
+
+import (
+    "fmt"
+    "testing"
+    "net/http"
+    "net/http/httptest"
+
+    "github.com/stretchr/testify/assert"
+)
+
+func mockServerForRequestUrlTest() (*httptest.Server) {
+    return mockServer(
+        func(responseWriter http.ResponseWriter, request *http.Request) {
+            fmt.Fprint(responseWriter, request.URL.String())
+        },
+    )
+}
+
+func mockServer(handlerFunction func (responseWriter http.ResponseWriter, request *http.Request)) (*httptest.Server) {
+    return httptest.NewServer(http.HandlerFunc(handlerFunction))
+}
+
+func TestCreateNewWLClient(t *testing.T) {
+    wlClient, err := NewClient("https://endpoint.api", "SenderId1234")
+    assert.Empty(t, err)
+    assert.Equal(t, "https://endpoint.api", wlClient.GetApiEndpoint())
+    assert.Equal(t, "SenderId1234", wlClient.GetSenderId())
+}
+
+func TestCreateNewWLClientWithEmptyApiEndpoint(t *testing.T) {
+    _, err := NewClient("", "SenderId1234")
+    assert.EqualError(t, err, "Empty API endpoint")
+}
+
+func TestCreateNewWLClientWithEmptySenderId(t *testing.T) {
+    _, err := NewClient("https://endpoint.api", "")
+    assert.EqualError(t, err, "Empty sender id")
+}
+
+func TestSuccessfulRequestRequestWithParameters(t *testing.T) {
+    server := mockServerForRequestUrlTest()
+    defer server.Close()
+
+    wlClient, _ := NewClient(server.URL, "SenderId1234")
+    responseString := "/action?sender=SenderId1234&key=value"
+
+    response, err := wlClient.Request("action", map[string]string{"key": "value"})
+    assert.Empty(t, err)
+    assert.Equal(t, []byte(responseString), response)
+}
+
+func TestSuccessfulRequestWithoutParameters(t *testing.T) {
+    server := mockServerForRequestUrlTest()
+    defer server.Close()
+
+    wlClient, _ := NewClient(server.URL, "SenderId4321")
+    responseString := "/action?sender=SenderId4321"
+
+    response, err := wlClient.Request("action", nil)
+    assert.Empty(t, err)
+    assert.Equal(t, []byte(responseString), response)
+}
+
+func TestRequestErrorWithEmptyAction(t *testing.T) {
+    server := mockServerForRequestUrlTest()
+    defer server.Close()
+
+    wlClient, _ := NewClient(server.URL, "SenderId1234")
+
+    _, err := wlClient.Request("", nil)
+    assert.EqualError(t, err,  "Empty action")
+}
